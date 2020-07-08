34310c9bba47ef819cfc99541623d57730def84e
Ilgiz <ilgizayupov7@gmail.com>
2020-07-08 13:49:28 +0500

    Создал миграцию и заполнил базу данных

--- my_first_project/news/models.py
+++ my_first_project/news/models.py
@@ -5,4 +5,5 @@ class News(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
     is_published = models.BooleanField(default=True)
