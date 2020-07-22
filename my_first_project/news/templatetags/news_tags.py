bbcad798b9c257eacb39756991cf427bd09fd10a
Ilgiz <ilgizayupov7@gmail.com>
2020-07-22 18:55:37 +0500

    Django ORM

--- my_first_project/news/templatetags/news_tags.py
+++ my_first_project/news/templatetags/news_tags.py
@@ -1,18 +1,19 @@
 from django import template
+from django.db.models import *
 
-from news.models import Category
+from news.models import Category, News
 
 register = template.Library() 
 
 
-@register.simple_tag(name='get_list_categories')
-def get_categories():
-    return Category.objects.all()
+# @register.simple_tag(name='get_list_categories')
+# def get_categories():
+#     return Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
 
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news__is_published'))
     return { 
         'categories': categories,
     }
