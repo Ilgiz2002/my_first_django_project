cb7c45ffd988e610045b4c112885f5659af0cdc7
Ilgiz <ilgizayupov7@gmail.com>
2020-07-24 12:33:32 +0500

    Теперь исправил ошибку :)

--- my_first_project/news/templatetags/news_tags.py
+++ my_first_project/news/templatetags/news_tags.py
@@ -13,7 +13,10 @@ register = template.Library()
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories():
-    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    categories = Category.objects.annotate(cat=Sum('news__is_published')).filter(cat__gt=0)
+    for item in categories:
+        item.cat = int(item.cat)
+
     return { 
         'categories': categories,
     }
