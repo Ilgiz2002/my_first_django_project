8b7cb559d13de44dabcec93bb68271c3eb993338
Ilgiz <ilgizayupov7@gmail.com>
2020-07-28 17:46:11 +0500

    Настроил форму входа

--- my_first_project/news/views.py
+++ my_first_project/news/views.py
@@ -3,26 +3,41 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 from django.contrib import messages
 
 from .models import News, Category
-from .forms import NewsForm, UserRegisterForm
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid(): 
-            form.save()
+            user = form.save()
             messages.success(request, 'Вы успешно зарегистрировались')
-            return redirect('login')
+            login(request, user)
+            return redirect('home')
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
         form = UserRegisterForm()
     return render(request, 'news/register.html', {'form':form})
 
-def login(request):
-    return render(request, 'news/login.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 class HomeNews(ListView):
     model = News
